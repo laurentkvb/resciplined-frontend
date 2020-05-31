9bc21fb3b0229ede2203b37e4767cd6fd7666bf3
laurentkvb <lkvb90@gmail.com>
2020-05-31 12:33:56 +0200

    Rescipilined url hardcoded

--- src/utils/quoteUrlHelper.tsx
+++ src/utils/quoteUrlHelper.tsx
@@ -4,5 +4,5 @@ export const quoteUrlHelper = (quote: IContentfulBase): string => {
   const quoteCategory = quote.category.slug;
   const quoteSlug = quote.id;
 
-  return `${window.location.origin.toString()}/${quoteCategory}/${quoteSlug}`;
+  return `https://resciplined.com/${quoteCategory}/${quoteSlug}`;
 };
